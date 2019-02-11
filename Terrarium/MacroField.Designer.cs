59f1e2b91c5b2f9016f058341f8dc1382159e08c
Taras K <taraskornuta@gmail.com>
2019-02-12 00:26:17 +0200

    add user control for macro wizard fields

---
+++ Terrarium/MacroField.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Terrarium
+{
+    partial class MacroField
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_MacroData = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_m1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_MacroData
+            // 
+            this.tb_MacroData.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_MacroData.Location = new System.Drawing.Point(3, 3);
+            this.tb_MacroData.Name = "tb_MacroData";
+            this.tb_MacroData.Size = new System.Drawing.Size(284, 24);
+            this.tb_MacroData.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(322, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(62, 24);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btn_m1
+            // 
+            this.btn_m1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.btn_m1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_m1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_m1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_m1.Location = new System.Drawing.Point(491, 3);
+            this.btn_m1.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.btn_m1.Name = "btn_m1";
+            this.btn_m1.Size = new System.Drawing.Size(85, 24);
+            this.btn_m1.TabIndex = 4;
+            this.btn_m1.Text = "M1";
+            this.btn_m1.UseVisualStyleBackColor = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(389, 3);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(69, 23);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(464, 8);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.checkBox2.Location = new System.Drawing.Point(292, 8);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.UseVisualStyleBackColor = false;
+            // 
+            // MacroField
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btn_m1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tb_MacroData);
+            this.DoubleBuffered = true;
+            this.MaximumSize = new System.Drawing.Size(580, 30);
+            this.MinimumSize = new System.Drawing.Size(580, 30);
+            this.Name = "MacroField";
+            this.Size = new System.Drawing.Size(580, 30);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox tb_MacroData;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_m1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
--- Terrarium/MacroPanelWizard.Designer.cs
+++ Terrarium/MacroPanelWizard.Designer.cs
@@ -28,13 +28,253 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.macroField3 = new Terrarium.MacroField();
+            this.macroField2 = new Terrarium.MacroField();
+            this.macroField1 = new Terrarium.MacroField();
+            this.macroField4 = new Terrarium.MacroField();
+            this.macroField5 = new Terrarium.MacroField();
+            this.macroField6 = new Terrarium.MacroField();
+            this.macroField7 = new Terrarium.MacroField();
+            this.macroField8 = new Terrarium.MacroField();
+            this.macroField9 = new Terrarium.MacroField();
+            this.macroField10 = new Terrarium.MacroField();
+            this.macroField11 = new Terrarium.MacroField();
+            this.macroField12 = new Terrarium.MacroField();
+            this.macroField17 = new Terrarium.MacroField();
+            this.macroField18 = new Terrarium.MacroField();
+            this.macroField19 = new Terrarium.MacroField();
+            this.macroField20 = new Terrarium.MacroField();
+            this.macroField21 = new Terrarium.MacroField();
+            this.macroField22 = new Terrarium.MacroField();
+            this.macroField23 = new Terrarium.MacroField();
+            this.macroField24 = new Terrarium.MacroField();
             this.SuspendLayout();
+            // 
+            // macroField3
+            // 
+            this.macroField3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField3.Location = new System.Drawing.Point(8, 72);
+            this.macroField3.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField3.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField3.Name = "macroField3";
+            this.macroField3.Size = new System.Drawing.Size(580, 30);
+            this.macroField3.TabIndex = 2;
+            // 
+            // macroField2
+            // 
+            this.macroField2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField2.Location = new System.Drawing.Point(8, 42);
+            this.macroField2.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField2.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField2.Name = "macroField2";
+            this.macroField2.Size = new System.Drawing.Size(580, 30);
+            this.macroField2.TabIndex = 1;
+            // 
+            // macroField1
+            // 
+            this.macroField1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField1.Location = new System.Drawing.Point(8, 12);
+            this.macroField1.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField1.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField1.Name = "macroField1";
+            this.macroField1.Size = new System.Drawing.Size(580, 30);
+            this.macroField1.TabIndex = 0;
+            // 
+            // macroField4
+            // 
+            this.macroField4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField4.Location = new System.Drawing.Point(8, 161);
+            this.macroField4.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField4.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField4.Name = "macroField4";
+            this.macroField4.Size = new System.Drawing.Size(580, 30);
+            this.macroField4.TabIndex = 5;
+            // 
+            // macroField5
+            // 
+            this.macroField5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField5.Location = new System.Drawing.Point(8, 131);
+            this.macroField5.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField5.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField5.Name = "macroField5";
+            this.macroField5.Size = new System.Drawing.Size(580, 30);
+            this.macroField5.TabIndex = 4;
+            // 
+            // macroField6
+            // 
+            this.macroField6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField6.Location = new System.Drawing.Point(8, 101);
+            this.macroField6.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField6.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField6.Name = "macroField6";
+            this.macroField6.Size = new System.Drawing.Size(580, 30);
+            this.macroField6.TabIndex = 3;
+            // 
+            // macroField7
+            // 
+            this.macroField7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField7.Location = new System.Drawing.Point(8, 340);
+            this.macroField7.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField7.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField7.Name = "macroField7";
+            this.macroField7.Size = new System.Drawing.Size(580, 30);
+            this.macroField7.TabIndex = 11;
+            // 
+            // macroField8
+            // 
+            this.macroField8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField8.Location = new System.Drawing.Point(8, 310);
+            this.macroField8.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField8.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField8.Name = "macroField8";
+            this.macroField8.Size = new System.Drawing.Size(580, 30);
+            this.macroField8.TabIndex = 10;
+            // 
+            // macroField9
+            // 
+            this.macroField9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField9.Location = new System.Drawing.Point(8, 280);
+            this.macroField9.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField9.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField9.Name = "macroField9";
+            this.macroField9.Size = new System.Drawing.Size(580, 30);
+            this.macroField9.TabIndex = 9;
+            // 
+            // macroField10
+            // 
+            this.macroField10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField10.Location = new System.Drawing.Point(8, 251);
+            this.macroField10.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField10.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField10.Name = "macroField10";
+            this.macroField10.Size = new System.Drawing.Size(580, 30);
+            this.macroField10.TabIndex = 8;
+            // 
+            // macroField11
+            // 
+            this.macroField11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField11.Location = new System.Drawing.Point(8, 221);
+            this.macroField11.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField11.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField11.Name = "macroField11";
+            this.macroField11.Size = new System.Drawing.Size(580, 30);
+            this.macroField11.TabIndex = 7;
+            // 
+            // macroField12
+            // 
+            this.macroField12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField12.Location = new System.Drawing.Point(8, 191);
+            this.macroField12.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField12.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField12.Name = "macroField12";
+            this.macroField12.Size = new System.Drawing.Size(580, 30);
+            this.macroField12.TabIndex = 6;
+            // 
+            // macroField17
+            // 
+            this.macroField17.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField17.Location = new System.Drawing.Point(8, 579);
+            this.macroField17.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField17.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField17.Name = "macroField17";
+            this.macroField17.Size = new System.Drawing.Size(580, 30);
+            this.macroField17.TabIndex = 19;
+            // 
+            // macroField18
+            // 
+            this.macroField18.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField18.Location = new System.Drawing.Point(8, 549);
+            this.macroField18.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField18.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField18.Name = "macroField18";
+            this.macroField18.Size = new System.Drawing.Size(580, 30);
+            this.macroField18.TabIndex = 18;
+            // 
+            // macroField19
+            // 
+            this.macroField19.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField19.Location = new System.Drawing.Point(8, 519);
+            this.macroField19.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField19.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField19.Name = "macroField19";
+            this.macroField19.Size = new System.Drawing.Size(580, 30);
+            this.macroField19.TabIndex = 17;
+            // 
+            // macroField20
+            // 
+            this.macroField20.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField20.Location = new System.Drawing.Point(8, 489);
+            this.macroField20.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField20.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField20.Name = "macroField20";
+            this.macroField20.Size = new System.Drawing.Size(580, 30);
+            this.macroField20.TabIndex = 16;
+            // 
+            // macroField21
+            // 
+            this.macroField21.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField21.Location = new System.Drawing.Point(8, 459);
+            this.macroField21.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField21.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField21.Name = "macroField21";
+            this.macroField21.Size = new System.Drawing.Size(580, 30);
+            this.macroField21.TabIndex = 15;
+            // 
+            // macroField22
+            // 
+            this.macroField22.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField22.Location = new System.Drawing.Point(8, 430);
+            this.macroField22.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField22.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField22.Name = "macroField22";
+            this.macroField22.Size = new System.Drawing.Size(580, 30);
+            this.macroField22.TabIndex = 14;
+            // 
+            // macroField23
+            // 
+            this.macroField23.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField23.Location = new System.Drawing.Point(8, 400);
+            this.macroField23.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField23.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField23.Name = "macroField23";
+            this.macroField23.Size = new System.Drawing.Size(580, 30);
+            this.macroField23.TabIndex = 13;
+            // 
+            // macroField24
+            // 
+            this.macroField24.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.macroField24.Location = new System.Drawing.Point(8, 370);
+            this.macroField24.MaximumSize = new System.Drawing.Size(580, 30);
+            this.macroField24.MinimumSize = new System.Drawing.Size(580, 30);
+            this.macroField24.Name = "macroField24";
+            this.macroField24.Size = new System.Drawing.Size(580, 30);
+            this.macroField24.TabIndex = 12;
             // 
             // MacroPanelWizard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 542);
+            this.ClientSize = new System.Drawing.Size(591, 616);
+            this.Controls.Add(this.macroField17);
+            this.Controls.Add(this.macroField18);
+            this.Controls.Add(this.macroField19);
+            this.Controls.Add(this.macroField20);
+            this.Controls.Add(this.macroField21);
+            this.Controls.Add(this.macroField22);
+            this.Controls.Add(this.macroField23);
+            this.Controls.Add(this.macroField24);
+            this.Controls.Add(this.macroField7);
+            this.Controls.Add(this.macroField8);
+            this.Controls.Add(this.macroField9);
+            this.Controls.Add(this.macroField10);
+            this.Controls.Add(this.macroField11);
+            this.Controls.Add(this.macroField12);
+            this.Controls.Add(this.macroField4);
+            this.Controls.Add(this.macroField5);
+            this.Controls.Add(this.macroField6);
+            this.Controls.Add(this.macroField3);
+            this.Controls.Add(this.macroField2);
+            this.Controls.Add(this.macroField1);
             this.Name = "MacroPanelWizard";
             this.Text = "MacroPanelWizard";
             this.ResumeLayout(false);
@@ -42,5 +282,26 @@
         }
 
         #endregion
+
+        private MacroField macroField1;
+        private MacroField macroField2;
+        private MacroField macroField3;
+        private MacroField macroField4;
+        private MacroField macroField5;
+        private MacroField macroField6;
+        private MacroField macroField7;
+        private MacroField macroField8;
+        private MacroField macroField9;
+        private MacroField macroField10;
+        private MacroField macroField11;
+        private MacroField macroField12;
+        private MacroField macroField17;
+        private MacroField macroField18;
+        private MacroField macroField19;
+        private MacroField macroField20;
+        private MacroField macroField21;
+        private MacroField macroField22;
+        private MacroField macroField23;
+        private MacroField macroField24;
     }
 }
