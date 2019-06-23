3fe3e1c7d87dbb6fb45917b1d6a9fe99595b5d0f
Taras K <taraskornuta@gmail.com>
2019-06-23 23:25:01 +0300

    change findForm backcolor

--- Terrarium/FindForm.Designer.cs
+++ Terrarium/FindForm.Designer.cs
@@ -118,7 +118,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
             this.ClientSize = new System.Drawing.Size(318, 80);
             this.Controls.Add(this.lbl_FindCount);
             this.Controls.Add(this.btn_Find);
